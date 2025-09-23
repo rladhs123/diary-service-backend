f57fc2ac99e2c3ab34f4ded5d3f91c34f35b8f36
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-09-23 18:51:49 +0900

    init

---
+++ diary-service/src/main/java/com/team8/diary/DiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.team8.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryServiceApplication.class, args);
+	}
+
+}
