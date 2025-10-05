340e396904866dbb0a179e5c1738f2b269af3a2e
niceome <gylim7826@naver.com>
2025-10-05 14:54:33 +0900

    add
    dto, repository,  domain change

--- diary-service/src/main/java/com/team8/diary/domain/Member.java
+++ diary-service/src/main/java/com/team8/diary/domain/Member.java
@@ -1,20 +1,41 @@
 package com.team8.diary.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 public class Member {
-    @Id @GeneratedValue
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private long memberId;
 
+    @Column(name = "member_email", nullable = false, unique = true)
     private String memberEmail;
-    private String memberName;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "member_name", nullable = false)
+    private String memberName;
+
+
+    //new로 객체 생성 안됨. 그대신 Member.build()로 생성가능
+    @Builder
+    private Member(String memberEmail, String password, String memberName) {
+        this.memberEmail = memberEmail;
+        this.password = password;
+        this.memberName = memberName;
+    }
+
+    public static Member signUp(String email, String encodedPassword) {
+        return Member.builder()
+                .memberEmail(email)
+                .password(encodedPassword)
+                .build();
+    }
 }
