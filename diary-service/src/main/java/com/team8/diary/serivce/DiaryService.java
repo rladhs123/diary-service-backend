1bb7ff451bf755f5ee4433277071d7e098f7f297
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-10-20 16:54:29 +0900

    .

--- diary-service/src/main/java/com/team8/diary/serivce/DiaryService.java
+++
@@ -1,7 +0,0 @@
-package com.team8.diary.serivce;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class DiaryService {
-}
--- diary-service/src/main/java/com/team8/diary/service/DiaryService.java
+++ diary-service/src/main/java/com/team8/diary/service/DiaryService.java
@@ -33,8 +33,8 @@ public class DiaryService {
         return diary;
     }
 
-    @Transactional
-    public List<Diary> findDiaryByMemberId(Long memberId) {
-        diaryRepository.findDiaryList();
-    }
+//    @Transactional
+//    public List<Diary> findDiaryByMemberId(Long memberId) {
+//        diaryRepository.findDiaryList();
+//    }
 }
